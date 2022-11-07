5ba413b44a7139cea20935eab33c19afacf3cc7b
zhaolei <zhaolei02@coocaa.com>
2022-11-07 11:05:37 +0800

    Initial Commit

---
+++ screenRecordTest/screenRecordTest.h
@@ -0,0 +1,18 @@
+//
+//  screenRecordTest.h
+//  screenRecordTest
+//
+//  Created by zhaolei on 2022/11/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for screenRecordTest.
+FOUNDATION_EXPORT double screenRecordTestVersionNumber;
+
+//! Project version string for screenRecordTest.
+FOUNDATION_EXPORT const unsigned char screenRecordTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <screenRecordTest/PublicHeader.h>
+
+
